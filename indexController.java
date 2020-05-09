a72f8db9d2737411c32044cfac2c65557a353654
Omar Puc Ek <65086436+Omar-Puc@users.noreply.github.com>
2020-05-09 18:32:44 -0500

    Add files via upload

--- indexController.java
+++ indexController.java
@@ -3,14 +3,20 @@ package com.example.demo.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.models.Articulos;
 
+@RequestMapping ("/app")
+@Controller
 
 public class indexController {
+	
+	
 
 	@GetMapping ({"/index", "/", "/home", ""})
 	public String index(Model model) {
@@ -18,6 +24,13 @@ public class indexController {
 		model.addAttribute("titulo", "Bienvenidos a nuesntra pagina Web");
 		
 		return "index";
+	}
+	
+	@RequestMapping ("/listar")
+	public String listar (Model model) {
+		model.addAttribute("titulo", "Listado de articulos");
+		
+		return "listar";
 	}
 	
 	@ModelAttribute("articulos")
